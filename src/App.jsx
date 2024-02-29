f8e54c1af75f9f231366f7ede9891fb4138ea2bd
muhsinachipra <muhsinachipra@gmail.com>
2024-02-29 21:46:38 +0530

    navbar banner rowpost, tmdb api called and rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import NavBar from './Components/NavBar/NavBar'
 import './App.css'
+import NavBar from './Components/NavBar/NavBar'
+import Banner from './Components/Banner/Banner'
+import RowPost from './Components/RowPost/RowPost'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+      <RowPost />
     </div>
   )
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = "41ffc7bc90c67e074d10d05be63a263d";
+export const imageUrl = "https://image.tmdb.org/t/p/original";
