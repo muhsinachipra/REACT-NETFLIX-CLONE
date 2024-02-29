2a55321f56ec2aeece8ad151806cec5adc9bbff9
muhsinachipra <muhsinachipra@gmail.com>
2024-02-29 14:31:49 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  )
+}
+
+export default App
